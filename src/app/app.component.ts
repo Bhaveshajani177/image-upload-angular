4ea28af8e51bcafd29ab080298142acc8b51a4d0
Bhavesh Ajani <bhaveshajani177@gmail.com>
2021-04-15 22:20:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'imageUpload';
+  files: File[] = [];
+  isDisabled = true; 
+
+	onSelect(event) {
+		this.files.push(...event.addedFiles);
+    this.isDisabled = false;
+	}
+
+	onRemove(event) {
+		this.files.splice(this.files.indexOf(event), 1);
+    if(this.files.length === 0){
+      this.isDisabled = true;  
+    }
+	}
+  upload(){
+    this.files = []; 
+    this.isDisabled = true;
+  }
 }
